bc4338d5a014e6123a3939924e6a232782838d1e
Raimundas Sakalauskas <raimis@particle.io>
2019-10-29 18:29:51 +0200

    sort stuff when generating files and add datetime to generated file

--- tinker_strings/venv/iOS.py
+++ tinker_strings/venv/iOS.py
@@ -6,7 +6,7 @@
 import sys
 import json
 import io
-from datetime import date
+from datetime import datetime
 
 jsonPath = 'TinkerStrings.json'
 stringsPath = 'TinkerStrings.strings'
@@ -24,7 +24,7 @@ if (len(sys.argv) > 5):
     useObjC = sys.argv[5] == 'objc'
 
 def outputStrings(json, key, output):
-    for n in json:
+    for n in sorted(json):
         if isinstance(json[n], dict):
             key.append(n)
             outputStrings(json[n], key, output)
@@ -36,7 +36,7 @@ def outputStrings(json, key, output):
 
 
 def outputSwift(json, key, output):
-    for n in json:
+    for n in sorted(json):
         if isinstance(json[n], dict):
             output.write(u'%senum %s {\n' % ('\t' * len(key), n))
             key.append(n)
@@ -47,15 +47,14 @@ def outputSwift(json, key, output):
             output.write(u'%sstatic let %s = "%s.%s".%s\n' % ('\t' * len(key), n, '.'.join(key), n, methodName))
 
 def outputObjC(json, key, output):
-    for n in json:
+    for n in sorted(json):
         if isinstance(json[n], dict):
             key.append(n)
             outputObjC(json[n], key, output)
             key.pop()
         else:
-            output.write(u'#define %s_%s = NSLocalizedStringFromTable(@\"%s.%s\", @\"ParticleSetupStrings\", @\"\")' % ('_'.join(key), n, '.'.join(key), n))
+            output.write(u'#define %s_%s NSLocalizedStringFromTableInBundle(@\"%s.%s\", @\"ParticleSetupStrings\", [ParticleSetupMainController getResourcesBundle], @\"\")\n' % ('_'.join(key), n, '.'.join(key), n))
     output.write(u'\n')
-
 
 #open data as
 data = json.load(io.open(jsonPath, 'r', encoding='utf8'))
@@ -63,13 +62,13 @@ key = []
 
 #prepare for the output
 output = io.open(stringsPath, 'w', encoding='utf8')
-output.write(u'// Generated on %s by iOS.py\n\n' % date.today())
+output.write(u'// Generated on %s by iOS.py\n\n' % datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 outputStrings(data, key, output)
 output.close()
 
 #prepare for the output
 output = io.open(swiftPath, 'w', encoding='utf8')
-output.write(u'// Generated on %s by iOS.py\n\n' % date.today())
+output.write(u'// Generated on %s by iOS.py\n\n' % datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 if (useObjC == False):
     outputSwift(data, key, output)
 else:
